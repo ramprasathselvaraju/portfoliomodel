a08e8b7b97347eea911cbc0463f13932d0725bd1
ramprasathselvaraju <95614373+ramprasathselvaraju@users.noreply.github.com>
2022-11-03 12:35:43 +0530

    Delete src directory

--- src/views/Config/ApiDetails.js
+++
@@ -1,3 +0,0 @@
-export const CREATE_STOCK = 'http://localhost:5008/stock_creation_api/';
-export const GETSTOCKS = 'http://localhost:5008/stock_creation_api_list/';
-export const GETSTOCKSLIST = 'http://localhost:5008/object_api_list/';
